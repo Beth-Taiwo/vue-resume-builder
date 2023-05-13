8dd9d0c5c8add7db125f6db5aef040574490f746
Beth Taiwo <popoolataiwo22@gmail.com>
2023-05-13 12:35:07 +0000

    imported box icon

--- src/components/resumePreview/Header.vue
+++ src/components/resumePreview/Header.vue
@@ -2,7 +2,7 @@
   <header class="l-header" id="header">
     <nav class="nav bd-container">
       <a href="#" class="nav__logo">Doe</a>
-      <div class="nav__menu" id="nav-menu">
+      <div class="nav__menu" id="nav-menu" ref="navMenuRef">
         <ul class="nav__list">
           <li class="nav__item">
             <a href="#home" class="nav__link active-link">
@@ -43,7 +43,7 @@
           </li>
         </ul>
       </div>
-      <div class="nav__toggle" id="nav-toggle">
+      <div class="nav__toggle" id="nav-toggle" ref="navToggleRef">
         <i class="bx bx-grid-alt"></i>
       </div>
     </nav>
@@ -52,7 +52,26 @@
 
 <script>
 export default {
-  name: 'HeaderComponent'
+  name: 'HeaderComponent',
+
+  mounted() {
+    this.showMenu(this.$refs.navToggleRef.id, this.$refs.navMenuRef.id)
+  },
+  methods: {
+    showMenu(toggleId, navId) {
+      console.log(toggleId)
+      const toggle = document.getElementById(toggleId)
+      const nav = document.getElementById(navId)
+      console.log(nav, 'this is the togglle')
+      // validate that variables exist
+      if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+          // we  add the show-menu class to the div tag with the nav__menu class
+          nav.classList.toggle('show-menu')
+        })
+      }
+    }
+  }
 }
 </script>
 
