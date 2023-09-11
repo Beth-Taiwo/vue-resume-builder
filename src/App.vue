c12701d7da99e01e2f20ee70184fcb012441c9ee
Emmanuel Popoola <popoolakenny07@gmail.com>
2023-09-11 22:50:20 +0000

    named file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import ResumePreview from './components/resumePreview/index.vue'
+import ResumeForm from './components/ResumeForm/index.vue'
 </script>
 
 <template>
-  <ResumePreview />
+  <ResumeForm />
 </template>
 
 <style scoped></style>
